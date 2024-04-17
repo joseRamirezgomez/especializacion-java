83ce109e55c7e42a53a4fc74402c89f3974d1664
Jose Ramirez <joseramirezgomez00@gmail.com>
2024-04-17 17:03:42 -0500

    tipos primitivos y strings

--- primerjava/src/Main.java
+++ primerjava/src/Main.java
@@ -1,7 +1,25 @@
-    public class Main {
+public class Main {
 
-    public  static void main(String[] args) {
-        System.out.println("Hola mundo");
+    public static void main(String[] args) {
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incuidoPlan = true;
+        double notaDePelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        String sipnosis = """
+            Representa un futuro distópico en el que
+            la humanidad está atrapada sin saberlo dentro
+            de una realidad simulada llamada Matrix, que 
+            las máquinas inteligentes han creado para distraer
+            a los humanos mientras usan sus cuerpos como
+            fuente de energía en campos de cultivo.
+            fue lanzada en: """ + fechaDeLanzamiento 
+            + " Incluido " + incuidoPlan;
+
+        System.out.println(sipnosis);
     }
 
 }
---
+++ primerjava/src/operadores.java
@@ -0,0 +1,16 @@
+public class operadores {
+    public static void main(String[] args) {
+        int a = 10 + 5; // Atribuye el valor 15 a la variable a
+        int b = 10 - 5; // Atribuye el valor 5 a la variable b
+        int c = 10 * 5; // Atribuye el valor 50 a la variable c
+        int d = 10 / 5; // Atribuye el valor 2 a la variable d
+        int e = 10 % 3; // Atribuye el valor 1 a la variable ya que (el resto de la divición de 10 por 3
+                        // es 1)
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+
+}
